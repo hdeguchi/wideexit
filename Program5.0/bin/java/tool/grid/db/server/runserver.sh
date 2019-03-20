b9e19c1730831d485d9e0d77de2df13c8278174e
kurata <kurata@giacometti.local>
2019-03-21 00:22:41 +0900

    Delete runserver.sh

--- Program5.0/bin/java/tool/grid/db/server/runserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export JAVA_HOME=/usr/java/jre1.5.0_09
-
-$JAVA_HOME/bin/java -cp /home/sgeadmin/soars/program/db/hsqldb.jar org.hsqldb.Server -database /home/sgeadmin/soars/program/db/soarsgrid
