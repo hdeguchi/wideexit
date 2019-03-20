3a52e96563159086f049a96305508a5a0c6627d6
kurata <kurata@giacometti.local>
2019-03-21 00:22:09 +0900

    Delete animator_ja.bat

--- Program5.0/bin/java/application/animator/animator_ja.bat
+++
@@ -1,7 +0,0 @@
-set CLASSES_PATH=..\..\..\..\classes
-set LIBRARY_PATH=..\..\..\..\lib
-set MAIN_CLASS=soars.application.animator.main.Application
-
-set CP=%CLASSES_PATH%
-
-start javaw -Xmx512m -cp %CP% %MAIN_CLASS% -language ja %*
