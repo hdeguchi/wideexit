99b05247ad8ecabf4e29291b6c7216addc1787fa
kurata <kurata@giacometti.local>
2019-03-21 00:22:22 +0900

    Delete visualshell_en.bat

--- Program5.0/bin/java/application/visualshell/visualshell_en.bat
+++
@@ -1,7 +0,0 @@
-set CLASSES_PATH=..\..\..\..\classes
-set LIBRARY_PATH=..\..\..\..\lib
-set MAIN_CLASS=soars.application.visualshell.main.Application
-
-set CP=%CLASSES_PATH%;%LIBRARY_PATH%\spring\spring.jar;%LIBRARY_PATH%\spring\commons-logging.jar;%LIBRARY_PATH%\xerces\xercesImpl.jar;%LIBRARY_PATH%\xalan\xalan.jar;%LIBRARY_PATH%\j2ssh\j2ssh-ant.jar;%LIBRARY_PATH%\j2ssh\j2ssh-common.jar;%LIBRARY_PATH%\j2ssh\j2ssh-core.jar;%LIBRARY_PATH%\j2ssh\j2ssh-dameon.jar;%LIBRARY_PATH%\beanshell\bsh-2.0b4.jar
-
-start javaw -Xmx512m -cp %CP% %MAIN_CLASS% -language en %*
